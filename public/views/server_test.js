84bb59fdf2900bfb741a638fb078c329cc85949e
Priyanka Shenoy <priyankadshenoy@gmail.com>
2017-12-14 17:12:56 -0500

    Update server_test.js

--- public/views/server_test.js
+++ public/views/server_test.js
@@ -1,5 +1,5 @@
 // Twilio Credentials
-const accountSid = 'AC4862315f7277edb17212d420c89296ef';
+const accountSid = 'AC4862315f7277edb17212d420c89296ef';    // coming from my account
 const authToken = '07b0639b18589680e729717cee093b8b';
 
 // require the Twilio module and create a REST client
@@ -7,9 +7,9 @@ const client = require('twilio')(accountSid, authToken);
 
 client.messages
     .create({
-        to: '+16177857613',
-        from: '+12348135704',
-        body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+        to: '+1**********', // mobile number u need to send it to
+        from: '+12348135704', // twilio free number 
+        body: 'This is the ship that made the Kessel Run in fourteen parsecs?', // text message
     })
 .then(function(message){
     console.log(message.sid)
